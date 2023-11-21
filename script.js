b6f43151cd5c8f53284b7d3a97762a5e01e74d5d
Martin <martinmmbote@gmail.com>
2023-11-21 19:23:38 +0300

    update

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const donateButtonText = document.getElementById('donate-button-text');
 const projectionLink = document.getElementById('Projections-link');
 const thematicLink = document.getElementById('Thematic-link');
 const aboutLink = document.getElementById('About-link');
+const linkedInIcon = document.getElementById('linkedin-icon');
+const linkedInIcon2 = document.getElementById('linkedin-icon-2');
 
 //mouseover event listener (start) to amongUsButton
 donateButton.addEventListener('mouseover', function() {
@@ -57,4 +59,30 @@ aboutLink.addEventListener('mouseover', function() {
 aboutLink.addEventListener('mouseout', function() {
     aboutLink.style.color = 'white';
     aboutLink.style.top = '1.7vw';
+});
+
+
+
+
+//mouseover event listener (start)
+linkedInIcon.addEventListener('mouseover', function() {
+    linkedInIcon.style.top = '42.1vw';
+});
+
+//mouseout event listener (end) 
+linkedInIcon.addEventListener('mouseout', function() {
+    linkedInIcon.style.top = '42.5vw';
+});
+
+
+
+
+//mouseover event listener (start)
+linkedInIcon2.addEventListener('mouseover', function() {
+    linkedInIcon2.style.top = '93.4vw';
+});
+
+//mouseout event listener (end) 
+linkedInIcon2.addEventListener('mouseout', function() {
+    linkedInIcon2.style.top = '93.8vw';
 });
