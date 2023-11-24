4b0d7e139180f646d292e021d8fcf8138790a691
Martin <martinmmbote@gmail.com>
2023-11-24 17:42:14 +0300

    thematic programs buttons animated

--- script.js
+++ script.js
@@ -5,6 +5,81 @@ const thematicLink = document.getElementById('Thematic-link');
 const aboutLink = document.getElementById('About-link');
 const linkedInIcon = document.getElementById('linkedin-icon');
 const linkedInIcon2 = document.getElementById('linkedin-icon-2');
+const docImage = document.getElementById('doctor-image-id');
+const srhrContainer = document.getElementById('SRHR');
+const readMore1 = document.getElementById('read-more-1');
+const climateJusticeContainer = document.getElementById('climate-justice');
+const climateJusticeImage = document.getElementById('climate-justice-id');
+const readMore2 = document.getElementById('read-more-2');
+const peaceAndSecurityContainer = document.getElementById('Peace-and-security');
+const peaceAndSecurityImage = document.getElementById('peace-and-security-id');
+const readMore3 = document.getElementById('read-more-3');
+const economicGovernanceContainer = document.getElementById('economic-governance');
+const econimicGovernanceImage = document.getElementById('economic-governance-id');
+const readMore4 = document.getElementById('read-more-4');
+
+
+
+
+//mouseover event listener (start) Economic Governance
+economicGovernanceContainer.addEventListener('mouseover', function() {
+    econimicGovernanceImage.style.transform = 'scale(1.1)';
+    readMore4.style.color = '#00AB4F';
+});
+
+//mouseout event listener (end) Economic Governance
+economicGovernanceContainer.addEventListener('mouseout', function() {
+    econimicGovernanceImage.style.transform = 'scale(1)';
+    readMore4.style.color = '#E16162';
+});
+
+
+
+
+//mouseover event listener (start) Peace and Security
+peaceAndSecurityContainer.addEventListener('mouseover', function() {
+    peaceAndSecurityImage.style.transform = 'scale(1.1)';
+    readMore3.style.color = '#00AB4F';
+});
+
+//mouseout event listener (end) Peace and Security
+peaceAndSecurityContainer.addEventListener('mouseout', function() {
+    peaceAndSecurityImage.style.transform = 'scale(1)';
+    readMore3.style.color = '#E16162';
+});
+
+
+
+
+//mouseover event listener (start) Climate Justice
+climateJusticeContainer.addEventListener('mouseover', function() {
+    climateJusticeImage.style.transform = 'scale(1.1)';
+    readMore2.style.color = '#00AB4F';
+});
+
+//mouseout event listener (end) Climate Justice
+climateJusticeContainer.addEventListener('mouseout', function() {
+    climateJusticeImage.style.transform = 'scale(1)';
+    readMore2.style.color = '#E16162';
+});
+
+
+
+
+//mouseover event listener (start) SRHR
+srhrContainer.addEventListener('mouseover', function() {
+    docImage.style.transform = 'scale(1.1)';
+    readMore1.style.color = '#00AB4F';
+});
+
+//mouseout event listener (end) SRHR
+srhrContainer.addEventListener('mouseout', function() {
+    docImage.style.transform = 'scale(1)';
+    readMore1.style.color = '#E16162';
+});
+
+
+
 
 //mouseover event listener (start) to amongUsButton
 donateButton.addEventListener('mouseover', function() {
